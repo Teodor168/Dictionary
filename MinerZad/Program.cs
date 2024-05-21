e8be1fe6e44ebea914f795f6f92e04f1966f7e80
Teodor168 <teodor16iliev@gmail.com>
2024-05-21 21:53:24 +0300

    neshto

---
+++ MinerZad/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace MinerZad
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //iNPUT
+            Dictionary<string, int> minerbook = new Dictionary<string, int>();
+
+            string input = null;
+            int quantity = 0;
+
+            //ACTion
+            do
+            {
+                input = Console.ReadLine();
+                if (input=="stop")
+                {
+                    break;
+                }
+                quantity = int.Parse(Console.ReadLine());
+                minerbook.Add(input, quantity);
+                
+            } while (input != "stop");
+
+            foreach (var pair in minerbook)
+            {
+
+                Console.WriteLine($"{pair.Key} ======> {pair.Value}");
+            }
+        }
+    }
+}
---
+++ TelefonenUkazatel/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace TelefonenUkazatel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //iNPUT
+            Dictionary<string, string> phonebook = new Dictionary<string, string>();
+
+            string input = null;
+
+            //ACTion
+            do
+            {
+
+                input = Console.ReadLine();
+                string[] temp = (input.Split(' '));
+                if (temp[0] == "A")
+                {
+                    if (phonebook.ContainsKey(temp[1]))
+                    {
+                        phonebook.Remove(temp[1]);
+                    }
+                    phonebook.Add(temp[1], temp[2]);
+                }
+                else if (temp[0] == "S")
+                {
+                    bool check = phonebook.ContainsKey(temp[1]);
+                    if (check)
+                    {
+                        Console.WriteLine(temp[1]);
+                        Console.WriteLine(phonebook[temp[1]]);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Contact {temp[1]} does not exist. ");
+                    }
+                }
+
+            } while (input != "END");
+        }
+    }
+}
